11b11ba6e216ae37d685235be0fdcfd9a565e761
Nvejqygroqxk <44037680+Nvejqygroqxk@users.noreply.github.com>
2020-06-16 20:03:10 +0200

    handleKeyPress

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react'; 
 import {useSelector, useDispatch} from 'react-redux';
 import './App.css';
 import {kek} from './actions';
@@ -6,17 +6,31 @@ import {kek} from './actions';
 function App() {
   const dispatch = useDispatch();
   const x = useSelector (state => state.x1)
-  let audio = new Audio("https://drive.google.com/uc?id=1w6Ih52RKjQJ7D4WB26Ua_BEOLNRWlURO")
-  //let audio = new Audio("/x1.wav")
+  //let audio = new Audio("https://drive.google.com/uc?id=1w6Ih52RKjQJ7D4WB26Ua_BEOLNRWlURO")
+  let audio = new Audio("/x1.wav")
+
   const start = () => {
     const y=audio.cloneNode()
     y.play()
   }
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyPress);
+  });
+
+  const handleKeyPress = (event) => {
+    if(event.key === 'q'){
+      start();
+    }
+  }
+
   return (
     <div className="App">
       <button onClick={start}>
         one
       </button>
+      {//<input type="text" id="one" onKeyPress={start} />
+}
       <br/>
     </div>
   );
